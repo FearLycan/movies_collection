4deb0878b9ffac8084fbbc93f2b915db66b8ce0e
DamianBronczyk <damian.bronczyk@gmail.com>
2017-08-09 09:42:00 +0200

    add pagination

--- application/models/DbTable/Movie.php
+++ application/models/DbTable/Movie.php
@@ -4,4 +4,21 @@ class  Application_Model_DbTable_Movie extends Zend_Db_Table_Abstract
 {
     protected $_name = 'movie';
     protected $_primary = 'id';
+
+
+    public function listMovies()
+    {
+        $db = Zend_Db_Table::getDefaultAdapter();
+        $selectMovies = new Zend_Db_Select($db);
+
+        $selectMovies->from('movie');
+
+        return $selectMovies;
+    }
+
+
+    public function getShortDescription()
+    {
+        return substr($this->description, 0, 125) . '...';
+    }
 }
--- application/modules/default/controllers/MovieController.php
+++ application/modules/default/controllers/MovieController.php
@@ -8,8 +8,14 @@ class MovieController extends Saffron_AbstractController
         $this->view->headTitle('Movies');
 
         $model = new Application_Model_DbTable_Movie();
-        $movies = $model->fetchAll();
+        //$movies = $model->fetchAll();
+        $movies = $model->listMovies();
 
+        $paginator = new Zend_Paginator(new Zend_Paginator_Adapter_DbSelect($movies));
+        $paginator->setItemCountPerPage(1)
+            ->setCurrentPageNumber($this->_getParam('page', 1));
+
+        $this->view->paginator = $paginator;
 
         $this->view->assign([
             'movies' => $movies,
@@ -20,16 +26,15 @@ class MovieController extends Saffron_AbstractController
     {
         $this->view->headTitle('Add New Movie');
 
-
         $request = $this->getRequest();
         $form = new Application_Form_Add();
 
         if ($this->getRequest()->isPost() && $form->isValid($request->getPost())) {
-            $movie = new Application_Model_DbTable_Movie($form->getValues());
+            $movie = $form->getValues();
             $mapper = new Application_Model_MovieMapper();
             $mapper->save($movie);
 
-            //die(var_dump($movie));
+            //die(var_dump($form->getValues()));
             return $this->_helper->redirector('movie/index');
         }
 
@@ -37,9 +42,15 @@ class MovieController extends Saffron_AbstractController
 
     }
 
-    public function viewAction($id)
+    public function viewAction()
     {
 
+        $id = $this->_getParam('id');
+
+        $model = new Application_Model_DbTable_Movie();
+        $movie = $model->getAdapter()->quoteInto('id = ?', $id);
+
+        die(var_dump($movie));
     }
 
     public function editAction($id)
